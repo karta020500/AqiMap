b91bf075f1368ba3655830e0ebe1da26dd134c6b
Brian <nick@Nickde-MacBook-Pro.local>
2020-06-21 11:21:16 +0800

    second commit

--- public/js/standard.js
+++ public/js/standard.js
@@ -4,15 +4,17 @@ var dragZoom;
 var source;
 var vector;
 var vectorLayer;
-var vectorSource;
+var vectorSource = new ol.source.Vector({
+	features: []
+});;
 var isLogin = false;
 var vector_kml;
 
 var style_modify;
 
-function init() {
+function init(){
 	$('#upload').attr('disabled', true);
-	init_map(resize_map_style, map_event);
+	init_map(resize_map_style,map_event);
 
 }
 function resize_map_style() {
@@ -21,29 +23,29 @@ function resize_map_style() {
 	$("#map div.ol-scale-line.ol-unselectable").css("left", "auto").css("right", "5px");
 }
 
-function map_event() {
+    
+function map_event(){
 
-	center_map.on('click', function (e) {
+	center_map.on('click',function(e){
 
 	});
+
 }
 
-function init_map(callback01, callback02) {
-
+function init_map(callback01,callback02){
 
 	var center_scaleLineControl = new ol.control.ScaleLine({
-	});
+    });
 
-
-	var osm_baseLayer = new ol.layer.Tile({
-		visible: true,
-		preload: Infinity,
-		source: new ol.source.OSM({
-			key: 'AphfyWsW8T9h2m7UnPu0LVDRD93iuJOHQa51y_vDbBug7Zx5XPUJM3mcs9gIHIUr',
-			imagerySet: 'AerialWithLabels', //Aerial or 'Road', 'AerialWithLabels', etc.
-			maxZoom: 19
-		})
-	});
+    var OSM_baseLayer = new ol.layer.Tile({
+        visible: true,
+        preload: Infinity,
+        source: new ol.source.OSM({
+            key: 'AphfyWsW8T9h2m7UnPu0LVDRD93iuJOHQa51y_vDbBug7Zx5XPUJM3mcs9gIHIUr',
+            imagerySet: 'AerialWithLabels', //Aerial or 'Road', 'AerialWithLabels', etc.
+            maxZoom: 19
+        })
+    });
 
 	center_map = new ol.Map({
 		controls: ol.control.defaults({
@@ -51,26 +53,28 @@ function init_map(callback01, callback02) {
 				collapsible: false
 			})
 		}).extend([
-			new ol.control.OverviewMap(),
-			center_scaleLineControl,
-			new ol.control.Zoom({
-				className: 'custom-zoom'
-			})
+            new ol.control.OverviewMap(),
+            center_scaleLineControl,
+            new ol.control.Zoom({
+                className: 'custom-zoom'
+            })
 		]),
 		interactions: ol.interaction.defaults().extend([
 			new ol.interaction.DragRotateAndZoom(),
 			new ol.interaction.MouseWheelZoom()
 		]),
 
-		layers: [osm_baseLayer
+		layers: [OSM_baseLayer
 
 		],
 
 		target: "map",
 		view: new ol.View({
 
-			center: ol.proj.fromLonLat([120.6718112, 24.1502971]),
-			zoom: 7,
+			center: ol.proj.transform([121.617203, 25.055653], 'EPSG:4326', 'EPSG:900913'),
+			projection: "EPSG:900913",
+            extent: ol.proj.transform([114.024543, 20.4267532245471, 16474217.640085237, 5813315.416422882], 'EPSG:4326', 'EPSG:900913'),
+			zoom: 5,
 			maxZoom: 19,
 			minZoom: 2,
 			enbaleRotation: false
@@ -78,19 +82,16 @@ function init_map(callback01, callback02) {
 	});
 
 	source = new ol.source.Vector({
-		wrapX: false
-	});
-	vectorSource = new ol.source.Vector({
-		features: []
-	});
+        wrapX: false
+    });
 	vector = new ol.layer.Vector({
-		source: source
-	});
+        source: source
+    });
 	center_map.addLayer(vector);
 
 	vectorLayer = new ol.layer.Vector({
-		source: vectorSource
-	});
+        source: vectorSource
+    });
 	center_map.addLayer(vectorLayer);
 
 	style_modify = new ol.style.Style({
@@ -101,17 +102,21 @@ function init_map(callback01, callback02) {
 		fill: new ol.style.Fill({
 			color: [220, 249, 164, 0.5]
 		})
-	});
+    });
 
 	callback01();
 	callback02();
 }
 
-function clear_vector() {
+var polyCoords = [];
+
+function clear_vector()
+{
 	vectorSource.clear();
 }
 
 function addPointByCoord(Latitude, Longitude) {
+
 	var feature = new ol.Feature({
 		geometry: new ol.geom.Point(ol.proj.fromLonLat([parseFloat(Longitude), parseFloat(Latitude)])),
 	});
@@ -124,5 +129,3 @@ function addPointByCoord(Latitude, Longitude) {
 	feature.setStyle(iconStyle);
 	vectorSource.addFeature(feature);
 }
-
-
--- resources/views/data/aqi.blade.php
+++ resources/views/data/aqi.blade.php
@@ -1,26 +1,6 @@
 @if($aqis != null && $aqis->count() != 0)
     @foreach ($aqis as $aqi)
       <script>
-        /*
-        	var Feature = new ol.Feature({
-		          geometry: new ol.geom.Point(ol.proj.fromLonLat(['{{ $aqi->Longitude }}', '{{ $aqi->Latitude }}']))
-	        });
-
-          var iconStyle = new ol.style.Style({
-            image: new ol.style.Icon(({
-              src: './images/mask.png'
-            }))
-          });
-
-            var vectorSource = new ol.source.Vector({
-            	features: [Feature]
-            });
-            //vectorSource.addFeature(Feature);
-            var vectorLayer = new ol.layer.Vector({
-            	source: vectorSource
-            });
-            center_map.addLayer(vectorLayer
-          */
           addPointByCoord('{{ $aqi->Latitude }}', '{{ $aqi->Longitude }}');
       </script>
     @endforeach
